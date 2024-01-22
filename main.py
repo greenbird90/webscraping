072f398d3c8537c459d66fc6e0cb7bd0cafee462
greenbird90 <72909678+greenbird90@users.noreply.github.com>
2024-01-22 03:30:09 +0000

    update untuk komentar dari tiap fungsi

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
+#import tool webdriver via selenium
 from selenium import webdriver
 
-#test
-
+#membuat fungsi untuk membuat driver 
 def get_drvier():
-  # Set options to make browsing easier
+  #mengatur options agar browser mudah dijalankan
+  
+  #membuat kelas kosong 
   options = webdriver.ChromeOptions()
+  #menonaktifkan pop-up infobars agar tidak mengganggu script saat menjalankan browser
   options.add_argument("disable-infobars")
+  #akses pada saat tampilan normal / maksimal agar kontent tidak berubah
   options.add_argument("start-maximized")
+  #menghindari issue pada saat interaksi dengan browser di perangkat linux (repl)
   options.add_argument("disable-dev-shm-usage")
+  #disable fungsi sandbox agar browser tidak terbatas pada mode saat menjalankan script
   options.add_argument("no-sandbox")
+  #membantu selenium untuk menghindari deteksi script dari browser atau halaman web
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
 
+  #membuat variable driver dengan menggunakan chrome class dari webdriver
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  #membuat koneksi ke halaman web yang akan di scraping 
+  driver.get("https://muamalahemas.com/")
   return driver
 
 def main():
+  #memanggil fungsi get_drvier untuk menjalankan driver
   driver = get_drvier()
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  #membuat variabel untuk menyimpan data (xpath) dari halaman web
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/div/section[2]/div/div[2]/div/div/div/div[1]")
   return element.text
-
+#cetak hasil dari fungsi main 
 print(main())
